12ec6daa9591f0ee7886c78d608ce98c765b9a1f
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2023-09-19 12:25:43 +0300

    Add dir_stat() to get plugins list

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+std::vector<std::string> dir_stat(std::string path) {
+  std::vector<std::string> files = {};
+
+  for (const auto& entry : fs::directory_iterator(path)) {
+    if (fs::is_regular_file(entry)) {
+      files.push_back(entry.path());
+    }
+  }
+
+  return files;
+};
+
+// TODO: change this mystery type
+using operation_func = int;
+std::map<std::string, operation_func> load_plugins() {
+  std::vector<std::string> plugin_names = dir_stat("./plugins");
+
+  for (const auto& plugin_name : plugin_names) {
+    std::cout << plugin_name << std::endl;
+  }
+
+  // TODO: return a value
+};
 
 int main() {
   std::cout << "Calculator" << std::endl;
+
+  load_plugins();
+
   return 0;
 };
