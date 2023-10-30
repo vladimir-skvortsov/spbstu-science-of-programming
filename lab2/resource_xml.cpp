e64f9f49e752e36f4af155399d362ccf8e593525
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2023-10-30 08:16:23 +0300

    Add XML::Document iterator methods to Resource_XML

--- lab2/resource_xml.cpp
+++ lab2/resource_xml.cpp
@@ -6,3 +6,24 @@ std::unique_ptr<Resource_XML> Resource_XML::create(const std::string& path){
 Resource_XML::Resource_XML(const std::string& path) {
   document = std::make_unique<XML::Document>(path);
 };
+XML::Node::iterator Resource_XML::begin() {
+  return document->begin();
+};
+XML::Node::iterator Resource_XML::end() {
+  return document->end();
+};
+XML::Node::iterator Resource_XML::find(std::function<bool(XML::Node *node)> callback) {
+  return document->find(callback);
+};
+XML::Node::iterator Resource_XML::find_by_tag(const std::string& tag) {
+  return document->find_by_tag(tag);
+};
+XML::Node::iterator Resource_XML::find_by_value(const std::string& tag) {
+  return document->find_by_value(tag);
+};
+XML::Node::iterator Resource_XML::add(XML::Node::iterator it, XML::Node* node) {
+  return document->add(it, node);
+};
+bool Resource_XML::erase(XML::Node::iterator it) {
+  return document->erase(it);
+};
--- lab2/resource_xml.h
+++ lab2/resource_xml.h
@@ -11,6 +11,14 @@ class Resource_XML {
   public:
     static std::unique_ptr<Resource_XML> create(const std::string& path);
 
+    XML::Node::iterator begin();
+    XML::Node::iterator end();
+    XML::Node::iterator find(std::function<bool(XML::Node *node)> callback);
+    XML::Node::iterator find_by_tag(const std::string &tag);
+    XML::Node::iterator find_by_value(const std::string &value);
+    XML::Node::iterator add(XML::Node::iterator it, XML::Node *node);
+    bool erase(XML::Node::iterator it);
+
   private:
     std::unique_ptr<XML::Document> document;
 
