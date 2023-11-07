3c23f3b9b93cad885c7aca86e2710138a8cbc376
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2023-11-07 18:21:18 +0300

    feat: check if a command was already registered

--- lab3/Engine.hpp
+++ lab3/Engine.hpp
@@ -10,6 +10,10 @@ class Engine {
   public:
     template<typename ClassType, typename... Args>
     void register_command(Wrapper<ClassType, Args...>* wrapper, const std::string& command) {
+      if (commands.find(command) != commands.end()) {
+        throw std::runtime_error("Command " + command + "is  already registered");
+      }
+
       commands[command] = [wrapper](std::unordered_map<std::string, int> args) {
         return (*wrapper)(std::move(args));
       };
